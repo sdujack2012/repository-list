33b849161307670b0a9544069484fe3f6d40c4ba
Kai Jiang <kai.jiang@gmail.com>
2020-06-21 15:30:04 +1000

    create reducer and action

---
+++ app/core/actionsAndReducers.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import produce from 'immer';
+
+const repositoriesSlice = createSlice({
+  name: 'repositories',
+  initialState: { repositories: [] },
+  reducers: {
+    updateRepositories: (state, action) =>
+      produce(state, stateDraft => {
+        stateDraft.repositories = action.payload;
+      }),
+  },
+});
+
+export const actions = repositoriesSlice.actions;
+
+export const rootReducer = combineReducers({
+  repositories: repositoriesSlice.reducer,
+});
+
+export const initialState = {
+  repositories: {
+    repositories: [],
+  },
+};
